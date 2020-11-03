7ac67d499db225fdc3702d5c285720b9fefa213b
divyesh1612 <divyesh1612@users.noreply.github.com>
2020-11-03 13:40:28 -0500

    Getuser and Getusers methods (#3)
    * Datalayer methods Getuser and Getusers
    
    * Added GetUser and GetUsers

---
+++ LoginAdmin.Data/DataObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace LoginAdmin.Data
+{
+    public class DataObject
+    {
+        public DataObject()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["adminconnect"].ConnectionString;
+            SqlTransaction = null;
+        }
+
+        public DataObject(SqlTransaction trans)
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["adminconnect"].ConnectionString;
+            SqlTransaction = trans;
+        }
+
+        private string _connectionString;
+
+        public string ConnectionString => _connectionString;
+
+        public SqlTransaction SqlTransaction { get; set; }
+    }
+}
