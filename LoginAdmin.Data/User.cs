d03b62b057172a5d79d385e4f8ad14c16bf74905
divyesh1612 <divyesh1612@users.noreply.github.com>
2020-11-04 14:41:43 -0500

    Updated User class

--- LoginAdmin.Data/User.cs
+++ LoginAdmin.Data/User.cs
@@ -10,15 +10,64 @@ namespace LoginAdmin.Data
 {
     public class User : DataObject
     {
-
         private int id;
+        private string firstname;
+        private string lastname;
+        private string address;
+        private string city;
+        private string province;
+        private string country;
+        private string phonenumber;
         private string email;
         private string password;
+        private int isadmin;
 
         public int Id
         {
             get => id;
             set => id = value;
+        }
+
+        public string FirstName
+        {
+            get => firstname;
+            set => firstname = value;
+        }
+
+        public string LastName
+        {
+            get => lastname;
+            set => lastname = value;
+        }
+
+        public string Address
+        {
+            get => address;
+            set => address = value;
+        }
+
+        public string City
+        {
+            get => city;
+            set => city = value;
+        }
+
+        public string Province
+        {
+            get => province;
+            set => province = value;
+        }
+
+        public string Country
+        {
+            get => country;
+            set => country = value;
+        }
+
+        public string PhoneNumber
+        {
+            get => phonenumber;
+            set => phonenumber = value;
         }
 
         public string Email
@@ -31,6 +80,12 @@ namespace LoginAdmin.Data
         {
             get => password;
             set => password = value;
+        }
+
+        public int IsAdmin
+        {
+            get => isadmin;
+            set => isadmin = value;
         }
 
         public User() : base()
@@ -47,24 +102,40 @@ namespace LoginAdmin.Data
         {
             ResetProperties();
             var data = GetUser(id);
+            Id = data.Id;
+            FirstName= data.FirstName;
+            LastName = data.LastName;
+            Address = data.Address;
+            City = data.City;
+            Province = data.Province;
+            Country = data.Country;
+            PhoneNumber = data.PhoneNumber;
             Email = data.Email;
             Password = data.Password;
-            Id = data.Id;
+            IsAdmin = data.IsAdmin;
+
         }
 
         private void ResetProperties()
-        {
+        {  
+            Id = -1;
+            FirstName = "";
+            LastName = "";
+            Address = "";
+            City = "";
+            Province = "";
+            PhoneNumber = "";
             Email = "";
             Password = "";
-            Id = -1;
+            IsAdmin = -1;
         }
 
         public User GetUser(int id)
         {
             User user = new User();
-            SqlConnection conn = new SqlConnection(user.ConnectionString);
+            SqlConnection con = new SqlConnection(ConnectionString);
 
-            SqlCommand cmd = new SqlCommand("dbo.RetrieveUserInfo", conn)
+            SqlCommand cmd = new SqlCommand("dbo.RetrieveUserInfo", con)
             {
                 CommandType = CommandType.StoredProcedure,
                 Transaction = SqlTransaction
@@ -72,7 +143,7 @@ namespace LoginAdmin.Data
 
             cmd.Parameters.AddWithValue("@Id", id);
 
-            conn.Open();
+            con.Open();
 
             SqlDataAdapter sa = new SqlDataAdapter(cmd);
 
@@ -88,7 +159,7 @@ namespace LoginAdmin.Data
             }
             finally
             {
-                conn.Close();
+                con.Close();
             }
 
             var dataRowCollection = ds.Tables[0]?.Rows;
@@ -96,8 +167,16 @@ namespace LoginAdmin.Data
                 foreach (DataRow dataRow in dataRowCollection)
                 {
                     user.Id = Convert.ToInt32(dataRow["Id"]);
+                    user.FirstName = dataRow["FirstName"].ToString();
+                    user.LastName = dataRow["LastName"].ToString();
+                    user.Address = dataRow["Address"].ToString();
+                    user.City = dataRow["City"].ToString();
+                    user.Province = dataRow["Province"].ToString();
+                    user.Country = dataRow["Country"].ToString();
+                    user.PhoneNumber = dataRow["PhoneNumber"].ToString();
                     user.Email = dataRow["Email"].ToString();
                     user.Password = dataRow["Password"].ToString();
+                    user.IsAdmin = Convert.ToInt32(dataRow["IsAdmin"]);
                 }
 
             return user;
@@ -111,14 +190,14 @@ namespace LoginAdmin.Data
             StringBuilder sb = new StringBuilder();
             sb.Append("select * from UserInfo");
 
-            SqlConnection conn = new SqlConnection(ConnectionString);
+            SqlConnection con = new SqlConnection(ConnectionString);
 
-            SqlCommand cmd = new SqlCommand(sb.ToString(), conn)
+            SqlCommand cmd = new SqlCommand(sb.ToString(), con)
             {
                 Transaction = SqlTransaction
             };
 
-            conn.Open();
+            con.Open();
 
             SqlDataAdapter sa = new SqlDataAdapter(cmd);
 
@@ -134,7 +213,7 @@ namespace LoginAdmin.Data
             }
             finally
             {
-                conn.Close();
+                con.Close();
             }
 
             var dataRowCollection = ds.Tables[0]?.Rows;
@@ -144,8 +223,17 @@ namespace LoginAdmin.Data
                     User user = new User
                     {
                         Id = Convert.ToInt32(dataRow["Id"]),
+                        FirstName = dataRow["FirstName"].ToString(),
+                        LastName = dataRow["LastName"].ToString(),
+                        Address = dataRow["Address"].ToString(),
+                        City = dataRow["City"].ToString(),
+                        Province = dataRow["Province"].ToString(),
+                        Country = dataRow["Country"].ToString(),
+                        PhoneNumber = dataRow["PhoneNumber"].ToString(),
                         Email = dataRow["Email"].ToString(),
-                        Password = dataRow["Password"].ToString()
+                        Password = dataRow["Password"].ToString(),
+                        IsAdmin = Convert.ToInt32(dataRow["IsAdmin"]),
+
                     };
                     users.Add(user);
                 }
@@ -153,5 +241,92 @@ namespace LoginAdmin.Data
             return users;
 
         }
+        public bool AddUser(User user)
+        {
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            SqlCommand cmd = new SqlCommand("dbo.CreateUserInfo", con)
+            {
+                CommandType = CommandType.StoredProcedure,  
+                Transaction = SqlTransaction
+
+            };
+
+            cmd.Parameters.AddWithValue("@id", user.Id);
+            cmd.Parameters.AddWithValue("@firstName", user.FirstName);
+            cmd.Parameters.AddWithValue("@lastName", user.LastName);
+            cmd.Parameters.AddWithValue("@address", user.Address);
+            cmd.Parameters.AddWithValue("@city", user.City);
+            cmd.Parameters.AddWithValue("@province", user.Province);
+            cmd.Parameters.AddWithValue("@country", user.Country);
+            cmd.Parameters.AddWithValue("@phoneNumer", user.PhoneNumber);
+            cmd.Parameters.AddWithValue("@isAdmin", user.IsAdmin);
+
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+        public bool UpdateUser(User user)
+        {   
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            SqlCommand cmd = new SqlCommand("dbo.UpdateUserInfo", con)
+            {
+                CommandType = CommandType.StoredProcedure,  
+                Transaction = SqlTransaction
+            
+            };
+
+            cmd.Parameters.AddWithValue("@id", user.Id);
+            cmd.Parameters.AddWithValue("@firstName", user.FirstName);
+            cmd.Parameters.AddWithValue("@lastName", user.LastName);
+            cmd.Parameters.AddWithValue("@address", user.Address);
+            cmd.Parameters.AddWithValue("@city", user.City);
+            cmd.Parameters.AddWithValue("@province", user.Province);
+            cmd.Parameters.AddWithValue("@country", user.Country);
+            cmd.Parameters.AddWithValue("@phoneNumer", user.PhoneNumber);
+            cmd.Parameters.AddWithValue("@isAdmin", user.IsAdmin);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+        public bool DeleteUser(int id)
+        {
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            SqlCommand cmd = new SqlCommand("dbo.DeleteUserInfo", con)
+            {
+                CommandType = CommandType.StoredProcedure,
+                Transaction = SqlTransaction
+            };
+        
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (result >= 1)
+                return true;
+            else
+                return false;
+        }
+
+
     }
 }
+
+
+  
