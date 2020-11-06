75f203d4e080896f1f736dc7ca8182edbfd417cd
harshgorjiwala <harsh7529gorjiwala@gmail.com>
2020-11-06 15:13:25 -0500

    Edited Views

--- LoginAdmin/Controllers/HomeController.cs
+++ LoginAdmin/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using LoginAdmin.Data;
-
 
 namespace LoginAdmin.Controllers
 {
@@ -12,21 +10,131 @@ namespace LoginAdmin.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new User().GetUsers());
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
+
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                throw new Exception("Username and password cannot be null or empty");
+            }
+
+            try
+            {
+                User user = new User(email);
+
+                if (user.Password == password)
+                {
+                    Session["userID"] = user.Id;
+                    Session["userName"] = user.Name;
+                    ViewBag.Success = "Login Succesfully";
+                    return View(user);
+                }
+                else
+                {
+                    throw new Exception("Invalid Login");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private string ComputeSha256Hash(object password)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            if (id <= 0)
+            {
+                throw new Exception("User id is not valid");
+            }
+            User user = new User(id);
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, User model)
+        {
+            if (id <= 0)
+            {
+                throw new Exception("User id is not valid");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+
+            try
+            {
+                //get that user from db
+                User user = new User(id);
+
+                // update based on the info provided
+                user.UpdateUser(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            ViewBag.success = "User updated succesfully";
+            return View(model);
+        }
+
+        /*public ActionResult Delete(int id)
+        {
+            if (id <= 0)
+            {
+                throw new Exception("User id is not valid");
+            }
+            User user = new User(id);
+
+            return View(user);
+        }*/
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            if (id <= 0)
+            {
+                throw new Exception("User id is not valid");
+            }
+
+            try
+            {
+                User user = new User(id);
+
+                user.DeleteUser(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            ViewBag.success = "User Deleted succesfully";
+            return View(user);
         }
     }
 }
